3dfa8a7e93bf7c270b9cc2f3a36926b393464484
ozaki <jp.ibis@gmail.com>
2022-03-08 15:25:00 +0900

    Armature全体リセット

--- ShowCtrlInPose.py
+++ ShowCtrlInPose.py
@@ -51,6 +51,31 @@ class ANIME_FACE_RIG_OT_show_all(bpy.types.Operator):
         return {'FINISHED'}
 
 
+# Reset All
+# =================================================================================================
+class ANIME_FACE_RIG_OT_show_all(bpy.types.Operator):
+    bl_idname = "anime_face_rig.reset_all"
+    bl_label = "Reset All"
+
+    # execute
+    def execute(self, context):
+        # 全レイヤー表示
+        for i in range(len(context.object.data.layers)):
+            context.object.data.layers[i] = True
+
+        # 全PoseBone表示
+        armature = bpy.context.active_object
+        for pose_bone in armature.pose.bones:
+            pose_bone.bone.hide = False
+
+        # 全ポーズのクリア
+        bpy.ops.pose.select_all(action='SELECT')
+        bpy.ops.pose.transforms_clear()
+        bpy.ops.pose.select_all(action='DESELECT')
+
+        return {'FINISHED'}
+
+
 # UI描画設定
 # =================================================================================================
 def ui_draw(context, layout):
@@ -59,3 +84,4 @@ def ui_draw(context, layout):
     layout.operator("anime_face_rig.show_deform_only")
     layout.operator("anime_face_rig.show_control_only")
     layout.operator("anime_face_rig.show_all")
+    layout.operator("anime_face_rig.reset_all")
