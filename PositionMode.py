d24261114f396004c3eac3a1805f606df033626e
oja-bitterlife <bitter.life.club@gmail.com>
2022-03-27 15:13:08 +0900

    REST/POSE切り替え追加

--- PositionMode.py
+++ PositionMode.py
@@ -1,17 +1,6 @@
 import bpy
 
 
-# To Rest Position
-# =================================================================================================
-class ANIME_POSE_TOOLS_OT_to_rest(bpy.types.Operator):
-    bl_idname = "anime_pose_tools.to_rest"
-    bl_label = "Set Rest Position"
-
-    # execute
-    def execute(self, context):
-        armature = bpy.context.active_object
-        return {'FINISHED'}
-
 # To Pose Position
 # =================================================================================================
 class ANIME_POSE_TOOLS_OT_to_pose(bpy.types.Operator):
@@ -20,7 +9,25 @@ class ANIME_POSE_TOOLS_OT_to_pose(bpy.types.Operator):
 
     # execute
     def execute(self, context):
-        armature = bpy.context.active_object
+        for obj in bpy.context.selected_objects:
+            if obj.type == "ARMATURE":
+                obj.data.pose_position = 'POSE'
+
+        return {'FINISHED'}
+
+
+# To Rest Position
+# =================================================================================================
+class ANIME_POSE_TOOLS_OT_to_rest(bpy.types.Operator):
+    bl_idname = "anime_pose_tools.to_rest"
+    bl_label = "Set Rest Position"
+
+    # execute
+    def execute(self, context):
+        for obj in bpy.context.selected_objects:
+            if obj.type == "ARMATURE":
+                obj.data.pose_position = 'REST'
+
         return {'FINISHED'}
 
 
@@ -29,5 +36,5 @@ class ANIME_POSE_TOOLS_OT_to_pose(bpy.types.Operator):
 def ui_draw(context, layout):
     layout.label(text="Change Position Mode:")
     box = layout.box()
-    box.operator("anime_pose_tools.to_rest")
     box.operator("anime_pose_tools.to_pose")
+    box.operator("anime_pose_tools.to_rest")
