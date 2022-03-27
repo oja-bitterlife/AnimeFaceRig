51ff6fabeb9693026c3c3bd9a63592af19076857
oja-bitterlife <bitter.life.club@gmail.com>
2022-03-27 16:10:26 +0900

    選択中Deformボーンを頂点グループに追加

--- WeightUtil.py
+++ WeightUtil.py
@@ -13,12 +13,12 @@ class ANIME_POSE_TOOLS_OT_remove_deform(bpy.types.Operator):
         deforms = [bone.name for armature in bpy.data.armatures for bone in armature.bones if bone.use_deform]
 
         # 選択中オブジェクトからdeformボーンの頂点グループをすべて削除する
-        for obj in context.selected_objects:
-            if obj.type == "MESH":
-                for vg in obj.vertex_groups:
+        for mesh in context.selected_objects:
+            if mesh.type == "MESH":
+                for vg in mesh.vertex_groups:
                     # deformボーンの頂点グループだった
                     if vg.name in deforms:
-                        obj.vertex_groups.remove(vg)
+                        mesh.vertex_groups.remove(vg)
 
         return {'FINISHED'}
 
@@ -31,6 +31,27 @@ class ANIME_POSE_TOOLS_OT_add_groups_from_bones(bpy.types.Operator):
 
     # execute
     def execute(self, context):
+        # defromボーンのみ追加するためリストアップ
+        deforms = [bone.name for armature in bpy.data.armatures for bone in armature.bones if bone.use_deform]
+
+        # 追加する頂点グループ名の取得
+        add_vg_names = []
+        for armature in context.selected_objects:
+            if armature.type == "ARMATURE":
+                for bone in armature.data.bones:
+                    # 選択中のdefromボーン
+                    if bone.select and bone.name in deforms:
+                        add_vg_names.append(bone.name)
+
+        # 頂点グループを追加
+        for mesh in context.selected_objects:
+            if mesh.type == "MESH":
+                # すでに存在する頂点グループ名取得
+                vg_names = [vg.name for vg in mesh.vertex_groups]
+                for add_name in add_vg_names:
+                    # 存在しない頂点グループ名だけ追加
+                    if add_name not in vg_names:
+                        mesh.vertex_groups.new(name=add_name)
 
         return {'FINISHED'}
 
@@ -38,7 +59,7 @@ class ANIME_POSE_TOOLS_OT_add_groups_from_bones(bpy.types.Operator):
 # UI描画設定
 # =================================================================================================
 def ui_draw(context, layout):
-    layout.label(text="Change Position Mode:")
+    layout.label(text="Vertex Group Util:")
     box = layout.box()
     box.operator("anime_pose_tools.remove_deform")
     box.operator("anime_pose_tools.add_groups_from_bones")
