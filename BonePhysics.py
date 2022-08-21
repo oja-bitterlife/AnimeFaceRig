895ce18e4e43f5d9f9f5e237d1d955f87f471acb
oja-bitterlife <bitter.life.club@gmail.com>
2022-08-21 18:55:36 +0900

    箱修正

--- BonePhysics.py
+++ BonePhysics.py
@@ -7,8 +7,21 @@ from .Util import BoneUtil
 COLLISION_BOX_PREFIX = "APT_ColBox@"
 
 # 箱生成用
-BOX_VERTS = [(-0.5,-0.5,-0.5),(-0.5,0.5,-0.5),(0.5,0.5,-0.5),(0.5,-0.5,-0.5),(-0.5,-0.5,0.5),(-0.5,0.5,0.5),(0.5,0.5,0.5),(0.5,-0.5,0.5)]
-BOX_FACES = [(0,1,2,3), (4,5,6,7), (0,4,5,1), (1,5,6,2), (2,6,7,3), (3,7,4,0)]
+BOX_VERTS = [(0,-0.5,0),(0,0.5,0),  # 0-1
+            (-0.5,-0.5,-0.5),(0,-0.5,-0.5),(0.5,-0.5,-0.5),  # 2-4
+            (-0.5,-0.5,0),(0.5,-0.5,0),  # 5-6
+            (-0.5,-0.5,0.5),(0,-0.5,0.5),(0.5,-0.5,0.5),  # 7-9
+            (-0.5,0.5,-0.5),(0,0.5,-0.5),(0.5,0.5,-0.5),  # 10-12
+            (-0.5,0.5,0),(0.5,0.5,0),  # 13-14
+            (-0.5,0.5,0.5),(0,0.5,0.5),(0.5,0.5,0.5)]  # 15-17
+
+BOX_FACES = [(2,3,0,5),(3,4,6,0),(5,0,8,7),(0,6,9,8),  # 上面
+            (10,13,1,11),(11,1,14,12),(13,15,16,1),(1,16,17,14),  # 下面
+            (2,5,13,10),(5,7,15,13),
+            (4,12,14,6),(6,14,17,9),
+            (2,10,11,3),(3,11,12,4),
+            (7,8,16,15),(8,9,17,16)]
+
 
 # Physics setup to Bone
 # =================================================================================================
@@ -40,7 +53,8 @@ class ANIME_POSE_TOOLS_OT_create_collision_box(bpy.types.Operator):
 
             # Box作成
             mesh = bpy.data.meshes.new("BoxMesh")
-            box_verts = [(armature.matrix_world.to_3x3() @ edit_bone.matrix.to_3x3() @ Vector([v[0]*size, v[1]*size, v[2]*size])).xyz for v in BOX_VERTS]
+            rot_matrix = armature.matrix_world.to_3x3() @ edit_bone.matrix.to_3x3()
+            box_verts = [(rot_matrix @ Vector([v[0]*size, v[1]*size, v[2]*size])).xyz for v in BOX_VERTS]
             mesh.from_pydata(box_verts, [], BOX_FACES)
             mesh.update(calc_edges=True)
             obj = bpy.data.objects.new(COLLISION_BOX_PREFIX + edit_bone.name, mesh)
