0255448e24d4fe3e54e9937cf5d37c6289e867e3
oja-bitterlife <bitter.life.club@gmail.com>
2022-08-22 14:38:23 +0900

    選択拡張追加

--- SelectBones.py
+++ SelectBones.py
@@ -6,7 +6,7 @@ import re
 # =================================================================================================
 class ANIME_POSE_TOOLS_OT_select_bones_with_a_key(bpy.types.Operator):
     bl_idname = "anime_pose_tools.select_bones_with_a_key"
-    bl_label = "With A Key"
+    bl_label = "With A Key In Frame"
 
     # execute
     def execute(self, context):
@@ -37,9 +37,49 @@ class ANIME_POSE_TOOLS_OT_select_bones_with_a_key(bpy.types.Operator):
         return{'FINISHED'}
 
 
+# Listup Selected Bones
+# =================================================================================================
+class ANIME_POSE_TOOLS_OT_select_to_edge(bpy.types.Operator):
+    bl_idname = "anime_pose_tools.select_to_edge"
+    bl_label = "To Edge"
+
+    # execute
+    def execute(self, context):
+        for pose_bone in bpy.context.selected_pose_bones:
+            self.rec_select_children(pose_bone)
+        return{'FINISHED'}
+
+    def rec_select_children(self, pose_bone):
+        pose_bone.bone.select = True
+        for child in pose_bone.children:
+            self.rec_select_children(child)
+
+
+# =================================================================================================
+class ANIME_POSE_TOOLS_OT_select_to_top(bpy.types.Operator):
+    bl_idname = "anime_pose_tools.select_to_top"
+    bl_label = "To Top"
+
+    # execute
+    def execute(self, context):
+        for pose_bone in bpy.context.selected_pose_bones:
+            self.rec_select_children(pose_bone)
+        return{'FINISHED'}
+
+    def rec_select_children(self, pose_bone):
+        pose_bone.bone.select = True
+        if pose_bone.parent:
+            self.rec_select_children(pose_bone.parent)
+
+
 # UI描画設定
 # =================================================================================================
 def ui_draw(context, layout):
     layout.label(text="Select Bones:")
+
     box = layout.box()
+    row = box.row()
+    row.alignment = 'EXPAND'
+    row.operator("anime_pose_tools.select_to_edge")
+    row.operator("anime_pose_tools.select_to_top")
     box.operator("anime_pose_tools.select_bones_with_a_key")
